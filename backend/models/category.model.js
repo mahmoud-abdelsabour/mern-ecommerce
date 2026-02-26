ae79e7580bfe6eab01e2cf426f8450413775ef87
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2026-02-26 17:16:24 +0200

    models: Category

---
+++ backend/models/category.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: false,
+        unique: true
+    }
+})
+
+categorySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Category = mongoose.model('Category', categorySchema)
+
+module.exports = Category
