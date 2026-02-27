9f2c138455664fa8877bf8ea0b65eaf06f52b8f1
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2026-02-27 03:18:45 +0200

    models: Review

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -44,32 +44,6 @@ const productSchema = mongoose.Schema({
             }
         }
     },
-    reviews:  [
-        new mongoose.Schema(
-            {
-                rating: {
-                    type: Number,
-                    required: true,
-                    min: 1,
-                    max: 5
-                },
-                comment: {
-                    type: String,
-                    required: true
-                },
-                userId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "User",
-                    required: true
-                },
-                username: {
-                    type: String,
-                    required: true
-                }
-            },
-            { timestamps: true, _id: true }
-        )
-    ],
     stock: {
         type: Number,
         required: true,
---
+++ backend/models/review.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+reviewSchema.index({ user: 1, product: 1 }, { unique: true })
+
+reviewSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = Review
