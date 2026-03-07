1f97603175742298c1c32f136d659693da7cf359
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2026-03-08 01:04:05 +0200

    validators: User

--- backend/validators/auth.validator.js
+++ backend/validators/auth.validator.js
@@ -24,15 +24,6 @@ const loginSchema = Joi.object({
 
 })
 
-const updateProfileSchema = Joi.object({
-    firstName: Joi.string().trim(),
-    lastName: Joi.string().trim(),
-    username: Joi.string().min(3).max(30).trim(),
-    email: Joi.string().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).trim(),
-    phone: Joi.string().pattern(/^01[0125][0-9]{8}$/).min(11).trim(),
-    profilePhoto: Joi.string().trim().allow(null, "")
-}).min(1);
-
 const updatePasswordSchema = Joi.object({
     currentPassword: Joi.string().required(),
     newPassword: Joi
@@ -43,25 +34,10 @@ const updatePasswordSchema = Joi.object({
 
 })
 
-const createAddressSchema = Joi.object({
-    country: Joi.string().trim().required(),
-    city: Joi.string().trim().required(),
-    postalcode: Joi.string().trim().required(),
-    street: Joi.string().trim().required(),
-    building: Joi.string().trim().required(),
-    floor: Joi.number().required(),
-    special_mark: Joi.string().trim().allow("")
-});
 
-const updateAddressSchema = Joi.object({
-    country: Joi.string().trim(),
-    city: Joi.string().trim(),
-    postalcode: Joi.string().trim(),
-    street: Joi.string().trim(),
-    building: Joi.string().trim(),
-    floor: Joi.number(),
-    special_mark: Joi.string().trim().allow("")
 
-}).min(1)
-
-module.exports = { registerSchema, loginSchema, updateProfileSchema, createAddressSchema, updateAddressSchema };
+module.exports = { 
+    registerSchema,
+    loginSchema, 
+    updatePasswordSchema 
+};
