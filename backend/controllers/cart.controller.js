5e7873179f9589527b41543000ffc515e7570df0
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2026-03-16 16:43:45 +0200

    controllers: Cart

---
+++ backend/controllers/cart.controller.js
@@ -0,0 +1,39 @@
+const cartService = require('../services/cart.service')
+
+const addToCart = async (request, response) => {
+  const { productId, quantity } = request.body
+  const user = request.user
+
+  const cart = await cartService.addToCart({ productId, quantity, user })
+  return response.status(200).json(cart)
+}
+
+const removeFromCart = async (request, response) => {
+  const { productId } = request.params
+  const user = request.user
+
+  const cart = await cartService.removeFromCart({ productId, user })
+  return response.status(200).json(cart)
+}
+
+const decrementCartItem = async (request, response) => {
+  const { productId } = request.params
+  const { amount } = request.body
+  const user = request.user
+
+  const cart = await cartService.decrementCartItem({ productId, user, amount })
+  return response.status(200).json(cart)
+}
+
+const getCart = async (request, response) => {
+  const user = request.user
+  const cart = await cartService.getCart({ user })
+  return response.status(200).json(cart)
+}
+
+module.exports = {
+  addToCart,
+  removeFromCart,
+  decrementCartItem,
+  getCart
+}
